fe6cd4123be17c4bb31b1fc6f77f4c0408bcdf8f
Arcaedion <arcaedionlab@gmail.com>
2019-01-19 19:45:06 +0000

    Primeiro commit

---
+++ Assets/Movimento2DJogador.cs
@@ -0,0 +1,32 @@
+﻿using Arcaedion.DevDasGalaxias;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controle2D))]
+public class Movimento2DJogador : MonoBehaviour {
+
+    [SerializeField]
+    private float _velocidade = 30f;
+
+    private Controle2D _controle;
+    private float _movimentoHorizontal;
+    private bool _pulando;
+
+    private void Awake()
+    {
+        _controle = GetComponent<Controle2D>();
+    }
+
+    void Update () {
+        _movimentoHorizontal = Input.GetAxisRaw("Horizontal") * _velocidade;
+        if (Input.GetButtonDown("Jump"))
+            _pulando = true;
+	}
+
+    void FixedUpdate()
+    {
+        _controle.Movimento(_movimentoHorizontal * Time.fixedDeltaTime, _pulando);
+        _pulando = false;
+    }
+}
